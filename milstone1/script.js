83ad39aa8059e748ebaefae7bccade019085ec7d
shamir-coder <139058412+shamir-coder@users.noreply.github.com>
2024-11-10 13:14:02 +0500

    Add files via upload

---
+++ milstone1/script.js
@@ -0,0 +1,10 @@
+var toggleButton = document.getElementById('toggal-skills');
+var skills = document.getElementById('skills');
+toggleButton.addEventListener('click', function () {
+    if (skills.style.display === 'none') {
+        skills.style.display = 'block';
+    }
+    else {
+        skills.style.display = 'none';
+    }
+});
